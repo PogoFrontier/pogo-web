9a173d2d64f9e7c151bfa9e5b5a1636a780147de
Adib Khan <adib.khan@gmail.com>
2020-11-25 01:50:04 -0500

    simplify home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,3 @@
-import { HomepageCounter } from '@containers/counter/HomepageCounter'
-import Link from 'next/link'
-
 /**
  * Homepage
  */
@@ -8,10 +5,6 @@ const HomePage = () => {
   return (
     <main>
       <h1>Hello, world!</h1>
-      <Link href={'/reset'}>
-        <a>reset page</a>
-      </Link>
-      <HomepageCounter />
     </main>
   )
 }
