a49ae6f208f97a60af7c10622179d9cef4f8eedd
Adib Khan <adib.khan@gmail.com>
2020-11-29 01:33:33 -0500

    add exit button

--- pages/matchup/[room].tsx
+++ pages/matchup/[room].tsx
@@ -3,10 +3,17 @@ import { useRouter } from 'next/router'
 const MatchupPage = () => {
   const router = useRouter()
   const { room } = router.query
+
+  const toHome = () => {
+    router.push("/")
+  }
   
   return (
     <main>
-      <h1>Room Code: {room}</h1>
+      <header>
+        <h1>Room Code: {room}</h1>
+        <button onClick={toHome}>Exit</button>
+      </header>
     </main>
   )
 }
