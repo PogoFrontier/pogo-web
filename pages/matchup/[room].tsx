dda9818a2ddf997b44e66ff2a4c5d647ee6e0852
Adib Khan <adib.khan@gmail.com>
2020-11-29 06:06:14 -0500

    add team loading

--- pages/matchup/[room].tsx
+++ pages/matchup/[room].tsx
@@ -4,10 +4,17 @@ import SocketContext from '@context/SocketContext'
 import TeamContext from '@context/TeamContext'
 import { TeamMember } from 'types/team'
 import Team from '@components/team/Team'
+import { SERVER } from '@config/index'
 
 interface RoomJoinPayload {
   id: string,
   team: TeamMember[]
+}
+
+const parseJSON = (response: Response) => {
+  return response.text().then(function(text) {
+    return text ? JSON.parse(text) : {}
+  })
 }
 
 const MatchupPage = () => {
@@ -18,9 +25,28 @@ const MatchupPage = () => {
   const team: TeamMember[] = useContext(TeamContext)
 
   useEffect(() => {
-    socket.on("room_join", (payload: RoomJoinPayload) => {
-      setOpponentTeam(payload.team)
-    })
+    if (socket.connected) {
+      fetch(`${SERVER}/opponent/${room}/${socket.id}`)
+      .then((response) => parseJSON(response))
+      .then(data => {
+        if (data && data.length) {
+          setOpponentTeam(data)
+        }
+        return
+      })
+      socket.on("room_join", (payload: RoomJoinPayload) => {
+        setOpponentTeam(payload.team)
+      })
+      socket.on("room_leave", () => {
+        setOpponentTeam([])
+      })
+      socket.on("disconnected", () => {
+        socket.removeAllListeners()
+        toHome()
+      })
+    } else {
+      toHome()
+    }
   }, [])
 
   const toHome = () => {
@@ -33,7 +59,10 @@ const MatchupPage = () => {
         <h1>Room Code: {room}</h1>
         <button onClick={toHome}>Exit</button>
       </header>
-      <Team team={opponentTeam} />
+      {
+        opponentTeam.length > 0 &&
+        <Team team={opponentTeam} />
+      }
       <Team team={team} isPlayer />
     </main>
   )
