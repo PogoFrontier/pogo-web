7b80a2f68f6aa785428a2f45b857cd9f7f20222b
Adib Khan <adib.khan@gmail.com>
2020-11-29 05:07:40 -0500

    add team param to room socket

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -2,16 +2,19 @@ import { useContext, useState } from 'react'
 import { useRouter } from 'next/router'
 import SocketContext from '@context/SocketContext'
 import style from './form.module.scss'
+import TeamContext from '@context/TeamContext'
+import { TeamMember } from 'types/team'
 
 const Form: React.FunctionComponent = () => {
   const [room, setRoom] = useState("")
   const socket: SocketIOClient.Socket = useContext(SocketContext)
+  const team: TeamMember[] = useContext(TeamContext)
   const router = useRouter()
 
   function connect() {
     socket.on('connect', () => {
       // Connected, let's sign-up for to receive messages for this room
-      socket.emit('room', room);
+      socket.emit('room', { room, team });
     })
     socket.connect();
     router.push(`/matchup/${room}`)
