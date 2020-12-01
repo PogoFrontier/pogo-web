a9a8425e15fe924a4aa4d0e48eb77d5f0a56be4f
Adib Khan <adib.khan@gmail.com>
2020-12-01 17:26:33 -0500

    add socket codes

---
+++ types/socket.ts
@@ -0,0 +1,8 @@
+export enum CODE {
+  rooms = 'ROOM',
+  get_opponent = 'GET_OPPONENT',
+  room_leave = 'ROOM_LEAVE',
+  room_join = 'ROOM_JOIN',
+  team_submit = 'TEAM_SUBMIT',
+  team_confirm = 'TEAM_CONFIRM',
+}
