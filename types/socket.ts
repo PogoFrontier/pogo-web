dbb94cec4f89b0ed4f6053344399ca049e0ab601
Adib Khan <adib.khan@gmail.com>
2020-12-09 16:22:57 -0500

    add game types

--- types/socket.ts
+++ types/socket.ts
@@ -5,4 +5,8 @@ export enum CODE {
   room_join = 'ROOM_JOIN',
   team_submit = 'TEAM_SUBMIT',
   team_confirm = 'TEAM_CONFIRM',
+  ready_game = 'READY_GAME',
+  game_check = 'GAME_CHECK',
+  game_start = 'GAME_START',
+  turn = 'TURN',
 }
