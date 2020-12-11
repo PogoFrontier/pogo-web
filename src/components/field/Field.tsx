9f72f57356167081b7b608ff061f16cc1807ab44
Adib Khan <adib.khan@gmail.com>
2020-12-11 11:52:48 -0500

    add Field

---
+++ src/components/field/Field.tsx
@@ -0,0 +1,19 @@
+import Character, { CharacterProps } from "./Character"
+import style from './field.module.scss'
+
+interface FieldProps {
+  characters: [CharacterProps, CharacterProps]
+}
+
+const Field: React.FunctionComponent<FieldProps> = ({ characters }) => {
+  return (
+    <section className={style.root}>
+      <div className={style.player}>
+        <Character char={characters[0].char} status={characters[0].status} back={true} />
+      </div>
+      <Character char={characters[1].char} status={characters[1].status} />
+    </section>
+  )
+}
+
+export default Field
