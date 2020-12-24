02ca3d68891b157c238f9799f169896229c0ba88
Adib Khan <adib.khan@gmail.com>
2020-12-24 14:27:03 -0500

    switch to wss

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export const SERVER = 'https://pogo-web-backend.herokuapp.com/'
-export const WSS = 'ws://pogo-web-backend.herokuapp.com/'
+export const WSS = 'wss://pogo-web-backend.herokuapp.com/'
 
 // export const SERVER = 'https://localhost:3000'
 // export const WSS = 'ws://localhost:3000'
