a5216089dab728c50c5ed36a9c6561e93a6fef59
Jake <jarthur.jmj@gmail.com>
2021-01-01 18:47:03 -0500

    fixed eslint errs and made url config dynamic

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,14 @@
-export const SERVER = 'https://pogo-web-backend.herokuapp.com/'
-export const WSS = 'wss://pogo-web-backend.herokuapp.com/'
+// export const SERVER = 'https://pogo-web-backend.herokuapp.com/'
+// export const WSS = 'wss://pogo-web-backend.herokuapp.com/'
 
 // export const SERVER = 'http://localhost:3000/'
 // export const WSS = 'ws://localhost:3000'
+
+export const SERVER =
+  process.env.NODE_ENV === 'production'
+    ? 'https://pogo-web-backend.herokuapp.com/'
+    : 'wss://pogo-web-backend.herokuapp.com/'
+export const WSS =
+  process.env.NODE_ENV === 'production'
+    ? 'wss://pogo-web-backend.herokuapp.com/'
+    : 'ws://localhost:3000'
