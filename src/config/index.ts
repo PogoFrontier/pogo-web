d30194354ff42e9793f7a4bd00d28716f6fd1afe
Jake <jarthur.jmj@gmail.com>
2021-01-02 13:03:58 -0500

    fixed the server url

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,7 +7,7 @@
 export const SERVER =
   process.env.NODE_ENV === 'production'
     ? 'https://pogo-web-backend.herokuapp.com/'
-    : 'wss://pogo-web-backend.herokuapp.com/'
+    : 'http://localhost:3000/'
 export const WSS =
   process.env.NODE_ENV === 'production'
     ? 'wss://pogo-web-backend.herokuapp.com/'
