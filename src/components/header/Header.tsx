da78665be771582f74b873e213f1cf8e7026f96e
Adib Khan <adib.khan@gmail.com>
2021-01-04 22:35:02 -0500

    add nav collapse menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import style from './header.module.scss'
 import Link from 'next/link'
+import { Menu, MenuButton, MenuItem, MenuList, MenuLink } from '@reach/menu-button'
+import "@reach/menu-button/styles.css"
+import { Icon } from '@components/icon/Icon'
+import UserContext from '@context/UserContext'
 
 const Header: React.FunctionComponent = () => {
+  const user = useContext(UserContext)
+  const handleAuth = () => {
+    user.auth().logout() // No idea if this works cause the context isn't type defined
+  }
+
   return (
     <header className={style.header}>
       <div className={style.title}>Project Grookey</div>
@@ -22,10 +31,29 @@ const Header: React.FunctionComponent = () => {
             <a>Settings</a>
           </Link>
         </li>
-        <button className="btn btn-negative">
+        <button className="btn btn-negative" onClick={handleAuth}>
           Log out
         </button>
       </ul>
+      <div className={style.hamburger}>
+        <Menu>
+          <MenuButton className={style.menubutton}><Icon name="menu" size="medium" /></MenuButton>
+          <MenuList>
+            <Link href="/">
+              <MenuLink>Home</MenuLink>
+            </Link>
+            <Link href="/team">
+              <MenuLink>Team</MenuLink>
+            </Link>
+            <Link href="/settings">
+              <MenuLink>Settings</MenuLink>
+            </Link>
+            <MenuItem onSelect={handleAuth} className={style.logout}>
+              Log out
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </div>
     </header>
   )
 }
--- src/components/icon/icons/index.ts
+++ src/components/icon/icons/index.ts
@@ -1,1 +1,2 @@
 export { default as clock } from './clock.svg'
+export { default as menu } from './menu.svg'
