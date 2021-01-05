f7a50c661f46c4ae293a26a56be0862c8d2c5b1a
Adib Khan <adib.khan@gmail.com>
2021-01-04 21:38:39 -0500

    simplify header style

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,9 +4,7 @@ import style from './header.module.scss'
 const Header: React.FunctionComponent = () => {
   return (
     <header className={style.header}>
-      <div className={style.navbar}>
-        <div className={style.title}>Project Grookey</div>
-      </div>
+      <div className={style.title}>Project Grookey</div>
     </header>
   )
 }
