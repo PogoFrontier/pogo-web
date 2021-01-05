4a0b7b3deec2c4ca5f951230188d0918b17d2bd8
Adib Khan <adib.khan@gmail.com>
2021-01-04 21:59:47 -0500

    add desktop nav links

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import style from './header.module.scss'
+import Link from 'next/link'
 
 const Header: React.FunctionComponent = () => {
   return (
     <header className={style.header}>
       <div className={style.title}>Project Grookey</div>
+      <ul className={style.links}>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/team">
+            <a>Team</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/settings">
+            <a>Settings</a>
+          </Link>
+        </li>
+        <button className="btn btn-negative">
+          Log out
+        </button>
+      </ul>
     </header>
   )
 }
