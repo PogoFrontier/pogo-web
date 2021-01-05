9d9b275e59c7fc4d499260be0637661b4249a764
Adib Khan <adib.khan@gmail.com>
2021-01-04 21:39:00 -0500

    add layout component

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,15 @@
+import Header from "@components/header/Header"
+import style from './layout.module.scss'
+
+const Layout: React.FunctionComponent = ({ children }) => {
+  return (
+    <main className={style.root}>
+      <Header />
+      <div className={style.content}>
+        { children }
+      </div>
+    </main>
+  )
+}
+
+export default Layout
