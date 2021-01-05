bd312168dea798b626f70cb12ecd0901903740bb
Adib Khan <adib.khan@gmail.com>
2021-01-05 13:49:05 -0500

    use .env.local in firebase config

--- next.config.js
+++ next.config.js
@@ -45,6 +45,15 @@ const getBuildConfig = () => {
       })
       return config
     },
+    env: {
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+      FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+      FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+      FIREBASE_SENDER_ID: process.env.FIREBASE_SENDER_ID,
+      FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+      FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
+    }
   }
   return nextConfig
 }
--- src/firebase.ts
+++ src/firebase.ts
@@ -2,15 +2,14 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 
-// this should really be in env but I can't get it to work, help lol
 const config = {
-  apiKey: 'AIzaSyBb4FG2ieLWK_tAnOrYggqHFuyloqPumq0',
-  authDomain: 'project-grookey.firebaseapp.com',
-  projectId: 'project-grookey',
-  storageBucket: 'project-grookey.appspot.com',
-  messagingSenderId: '1019022204293',
-  appId: '1:1019022204293:web:e5640211111de2314478ec',
-  measurementId: 'G-TTL51XSGQT',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 }
 
 const app = !firebase.apps.length
