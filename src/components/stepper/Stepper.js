de52784283fc30fcb6e0064d2e42b6ad3fe9e47a
Abhishek Mishra <abhishek@digiqt>
2021-01-07 01:56:35 +0530

    Basic Charge Move Powerup Button

---
+++ src/components/stepper/Stepper.js
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+import { NICE, GREAT, EXCELLENT } from './constants'
+import style from './stepper.module.scss'
+
+const Stepper = () => {
+  const [stepperCharge, setStepperCharge] = useState(0)
+  const [stepperLabel, setStepperLabel] = useState('')
+
+  const onStepperClick = () => {
+    if (stepperCharge < 100) {
+      let stepperCount = stepperCharge
+      stepperCount += 10
+      setStepperCharge(stepperCount)
+      if (stepperCount >= 40 && stepperCount < 80) {
+        setStepperLabel(NICE)
+      } else if (stepperCount >= 80 && stepperCount < 100) {
+        setStepperLabel(GREAT)
+      } else if (stepperCount == 100) {
+        setStepperLabel(EXCELLENT)
+      }
+    }
+  }
+  return (
+    <div>
+      <button
+        onClick={() => onStepperClick()}
+        className={style[`button${stepperLabel}`]}
+      >
+        {stepperLabel === '' ? 'Tap to Charge' : stepperLabel}
+      </button>
+    </div>
+  )
+}
+
+export default Stepper
