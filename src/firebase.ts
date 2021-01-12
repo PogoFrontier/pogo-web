c9f60f2316fd2f1a4f8daccb4fd0445983205c92
Adib Khan <adib.khan@gmail.com>
2021-01-12 01:06:59 -0500

    fix config

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: 'AIzaSyBb4FG2ieLWK_tAnOrYggqHFuyloqPumq0',
+  authDomain: 'project-grookey.firebaseapp.com',
+  projectId: 'project-grookey',
+  storageBucket: 'project-grookey.appspot.com',
+  messagingSenderId: '1019022204293',
+  appId: '1:1019022204293:web:e5640211111de2314478ec',
+  measurementId: 'G-TTL51XSGQT',
 }
 
 const app = !firebase.apps.length
