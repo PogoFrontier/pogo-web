1d6195f060bb603ab988d6e3180afa00d97a26a4
Adib Khan <adib.khan@gmail.com>
2021-01-18 22:06:10 -0500

    add split component

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,13 +1,19 @@
 import Layout from '@components/layout/Layout'
+import Split from '@components/split/Split'
+import { TabPanel } from '@reach/tabs'
 import style from './style.module.scss'
 
 const SettingsPage = () => {
   return (
     <Layout>
       <main className={style.root}>
-        <h1>
-          Settings
-        </h1>
+        <Split tabs={["Controls", "Account"]}>
+          <TabPanel>
+            <h1>
+              Controls
+            </h1>
+          </TabPanel>
+        </Split>
       </main>
     </Layout>
   )
