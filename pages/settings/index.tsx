4895a46771a3c5ec679c00e4c4dd00804fb72f64
Adib Khan <adib.khan@gmail.com>
2021-01-18 21:17:10 -0500

    add settings page

---
+++ pages/settings/index.tsx
@@ -0,0 +1,16 @@
+import Layout from '@components/layout/Layout'
+import style from './style.module.scss'
+
+const SettingsPage = () => {
+  return (
+    <Layout>
+      <main className={style.root}>
+        <h1>
+          Settings
+        </h1>
+      </main>
+    </Layout>
+  )
+}
+
+export default SettingsPage
