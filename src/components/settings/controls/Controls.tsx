c9fafa4db5a2bde95da0ef5c5d2e1daed3da0c52
Adib Khan <adib.khan@gmail.com>
2021-01-19 19:28:28 -0500

    add controls functionality

--- src/components/settings/controls/Controls.tsx
+++ src/components/settings/controls/Controls.tsx
@@ -1,35 +1,128 @@
 import Input from "@components/input/Input"
+import SettingsContext from "@context/SettingsContext"
+import { useContext, useEffect, useState } from "react"
 import style from "./controls.module.scss"
 
+const returnKey = (key: string) => {
+  if (key === " ") {
+    return "space"
+  }
+  return key
+}
+
+interface KeyInputProps {
+  value: string
+  title: string
+  type: string
+  onSetKey: (keyType: string, newKey: string) => void
+}
+
+const KeyInput: React.FC<KeyInputProps> = ({ value, title, type, onSetKey }) => {
+  const [v, setV] = useState(value)
+
+  useEffect(() => {
+    setV(value)
+  }, [value])
+
+  const show = () => {
+    setV("")
+  }
+
+  const hide = () => {
+    if (v === "") {
+      setV(value)
+    }
+  }
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let val = e.target.value
+    if (val.length === 0) {
+      return
+    }
+    if (val.length !== 1) {
+      val = val[val.length - 1]
+    }
+    setV(val)
+    onSetKey(type, val)
+  }
+
+  return (
+    <>
+      <Input
+        title={title}
+        value={returnKey(v)}
+        onFocus={show}
+        onBlur={hide}
+        onChange={onChange}
+        highlighted={true}
+      />
+    </>
+  )
+}
+
 const Controls = () => {
+  const settings = useContext(SettingsContext)
+  const {
+    fastKey,
+    charge1Key,
+    charge2Key,
+    switch1Key,
+    switch2Key,
+    shieldKey
+  } = settings.keys
+
+  const onSetKey = (keyType: string, newKey: string) => {
+    const keysCopy = { ...settings.keys }
+    const oldKey = keysCopy[keyType]
+    for (const i of Object.keys(keysCopy)) {
+      if (keysCopy[i] === newKey) {
+        keysCopy[i] = oldKey
+      }
+    }
+    keysCopy[keyType] = newKey
+    settings.setKeys(keysCopy)
+  }
+
   return (
     <div className={style.root}>
       <section>
-        <Input
+        <KeyInput
           title="Fast Attack / Charge Move Powerup"
-          highlighted={true}
+          value={fastKey}
+          type="fastKey"
+          onSetKey={onSetKey}
         />
-        <Input
+        <KeyInput
           title="Switch 1"
-          highlighted={true}
+          value={switch1Key}
+          type="switch1Key"
+          onSetKey={onSetKey}
         />
-        <Input
+        <KeyInput
           title="Switch 2"
-          highlighted={true}
+          value={switch2Key}
+          type="switch2Key"
+          onSetKey={onSetKey}
         />
       </section>
       <section>
-        <Input
+        <KeyInput
           title="Shield"
-          highlighted={true}
+          value={shieldKey}
+          type="shieldKey"
+          onSetKey={onSetKey}
         />
-        <Input
+        <KeyInput
           title="Charge 1"
-          highlighted={true}
+          value={charge1Key}
+          type="charge1Key"
+          onSetKey={onSetKey}
         />
-        <Input
+        <KeyInput
           title="Charge 2"
-          highlighted={true}
+          value={charge2Key}
+          type="charge2Key"
+          onSetKey={onSetKey}
         />
       </section>
     </div>
