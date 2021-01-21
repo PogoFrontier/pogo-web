fdebaf48f7a6d8b69f1f78ed17b9b7d1fc2c5d25
Adib Khan <adib.khan@gmail.com>
2021-01-20 19:23:00 -0500

    add split view to team

--- pages/team/index.tsx
+++ pages/team/index.tsx
@@ -4,40 +4,17 @@ import React, { useContext, useEffect, useState } from 'react'
 import CraftTeam from '@components/craft_team/CraftTeam'
 import { updateUserTeam } from '@common/actions/userAPIActions'
 import getMini from '@common/actions/getMini'
+import Split from '@components/split/Split'
+import { TabPanel } from '@reach/tabs'
 
-const teamPage = () => {
+interface ContentProps {
+  meta: string
+}
+
+const Content: React.FC<ContentProps> = ({ meta }) => {
   const { user, refreshUser } = useContext(UserContext)
-  const [metas, setMetas] = useState([
-    'Great League',
-    'Ultra League',
-    'Master League',
-  ])
-  const [selectedMeta, setSelectedMeta] = useState('Great League')
   const [craftingTeam, setCraftingTeam] = useState(false)
   const [teamToEdit, setTeamToEdit] = useState<any | null>(null)
-
-  useEffect(() => {
-    if (user && metas) {
-      const metaArr: string[] = [
-        'Great League',
-        'Ultra League',
-        'Master League',
-      ]
-      user.teams.forEach((team: any) => {
-        if (!metaArr.some((m) => m === team.meta)) metaArr.push(team.meta)
-      })
-    }
-  }, [user, metas])
-
-  const addMeta = () => {
-    // open modal and add custom meta to list
-    // eventually these metas and their rules should be stored on backend, useEffect to store them in state
-    // they should also be part of the user object
-    setMetas([...metas, 'Holiday Cup'])
-    setCraftingTeam(false) // need this
-    setSelectedMeta('Holiday Cup') // will be whatever the new meta is
-    // reset select value on close/submit of modal
-  }
 
   const updateTeam = (team: any) => {
     if (localStorage.getItem('token')) {
@@ -56,11 +33,6 @@ const teamPage = () => {
     }
   }
 
-  const handleMetaChange = (e: any) => {
-    setCraftingTeam(false)
-    e.target.value === 'add-meta' ? addMeta() : setSelectedMeta(e.target.value)
-  }
-
   const handleOnClickAddTeam = () => {
     setCraftingTeam(true)
   }
@@ -70,95 +42,123 @@ const teamPage = () => {
     setCraftingTeam(true)
   }
 
-  const handleSetSelectedMeta = (e: any) => {
-    setSelectedMeta(e.target.value)
+  if (!user || !user.teams) {
+    return <p>Please sign in to use the teambuilder</p>
+  }
+
+  if (craftingTeam) {
+    return (
+      <div className="crafting-team">
+      {
+        teamToEdit
+        ? (
+          <CraftTeam
+            selectedMeta={meta}
+            updateTeam={updateTeam}
+            teamToEdit={teamToEdit}
+          />
+        )
+        : (
+          <CraftTeam
+            selectedMeta={meta}
+            updateTeam={updateTeam}
+          />
+        )
+      }
+    </div>
+    )
+  }
+
+  return (
+    <div className="team-list">
+      {
+        user.teams.length > 0
+        ? (
+          user.teams.map((team: any, i: number) => {
+            if (team.format === meta) {
+              return (
+                <div className="team-item" key={team.id}>
+                  <label className="team-name">{team.name}</label>
+                  <div className="team-box">
+                    {team.members.map((member: any, index: number) => (
+                      <img
+                        key={index}
+                        src={getMini(member.sid)}
+                        alt={member.speciesName}
+                      />
+                    ))}
+                  </div>
+                  <div className="team-btns">
+                    <button value={i} onClick={handleEditTeam}>
+                      Edit
+                    </button>
+                    <button>Delete</button>
+                  </div>
+                </div>
+              )
+            }
+          })
+        )
+        : <p>No Teams to display</p>
+      }
+      <button onClick={handleOnClickAddTeam}>Add Team</button>
+    </div>
+  )
+}
+
+const TeamPage = () => {
+  const { user } = useContext(UserContext)
+  const [ metas, setMetas ] = useState([
+    'Great League',
+    'Ultra League',
+    'Master League',
+  ])
+  const [ index, setIndex ] = useState(0)
+
+  useEffect(() => {
+    if (user && metas) {
+      const metaArr: string[] = [
+        'Great League',
+        'Ultra League',
+        'Master League',
+      ]
+      user.teams.forEach((team: any) => {
+        if (!metaArr.some((m) => m === team.meta)) metaArr.push(team.meta)
+      })
+    }
+  }, [user, metas])
+
+  const addMeta = () => {
+    // open modal and add custom meta to list
+    // eventually these metas and their rules should be stored on backend, useEffect to store them in state
+    // they should also be part of the user object
+    const newMetas = [...metas, 'Holiday Cup']
+    setMetas(newMetas)
+    setIndex(newMetas.length - 1) // will be whatever the new meta is
+    // reset select value on close/submit of modal
+  }
+
+  const tabs = metas.map(x => (
+    <TabPanel key={x}>
+      <Content meta={x} />
+    </TabPanel>
+  ))
+
+  const onChange = (i: number) => {
+    setIndex(i)
   }
 
   return (
     <Layout>
-      {user && user.teams && (
-        <div>
-          <div className="side-meta-menu">
-            <ul style={{ listStyleType: 'none' }}>
-              {metas &&
-                metas.map((meta: any) => (
-                  <li key={meta} value={meta} onClick={handleSetSelectedMeta}>
-                    {meta}
-                  </li>
-                ))}
-              <li className="add-meta-button">
-                <button onClick={addMeta}>Add New Meta</button>
-              </li>
-            </ul>
-          </div>
-          <div className="mobile-meta-menu">
-            <select
-              name="metas"
-              id="metas"
-              value={selectedMeta}
-              onChange={handleMetaChange}
-            >
-              {metas &&
-                metas.map((meta: any) => (
-                  <option key={meta} value={meta}>
-                    {meta}
-                  </option>
-                ))}
-              <option value="add-meta">Add New Meta</option>
-            </select>
-          </div>
-          {craftingTeam ? (
-            <div className="crafting-team">
-              {teamToEdit ? (
-                <CraftTeam
-                  selectedMeta={selectedMeta}
-                  updateTeam={updateTeam}
-                  teamToEdit={teamToEdit}
-                />
-              ) : (
-                <CraftTeam
-                  selectedMeta={selectedMeta}
-                  updateTeam={updateTeam}
-                />
-              )}
-            </div>
-          ) : (
-            <div className="team-list">
-              {user.teams.length > 0 ? (
-                user.teams.map((team: any, i: number) => {
-                  if (team.format === selectedMeta) {
-                    return (
-                      <div className="team-item" key={team.id}>
-                        <label className="team-name">{team.name}</label>
-                        <div className="team-box">
-                          {team.members.map((member: any, index: number) => (
-                            <img
-                              key={index}
-                              src={getMini(member.sid)}
-                              alt={member.speciesName}
-                            />
-                          ))}
-                        </div>
-                        <div className="team-btns">
-                          <button value={i} onClick={handleEditTeam}>
-                            Edit
-                          </button>
-                          <button>Delete</button>
-                        </div>
-                      </div>
-                    )
-                  }
-                })
-              ) : (
-                <p>No Teams to display</p>
-              )}
-              <button onClick={handleOnClickAddTeam}>Add Team</button>
-            </div>
-          )}
-        </div>
-      )}
+      <Split
+        tabs={metas}
+        buttonProps={{ title: "New Meta", onClick: addMeta }}
+        tabProps={{ index, onChange, children: null }}
+      >
+        {tabs}
+      </Split>
     </Layout>
   )
 }
 
-export default teamPage
+export default TeamPage
--- src/components/split/Split.tsx
+++ src/components/split/Split.tsx
@@ -1,15 +1,31 @@
-import { Tabs, TabList, Tab, TabPanels } from "@reach/tabs"
+import { Tabs, TabList, Tab, TabPanels, TabsProps } from "@reach/tabs"
 import "@reach/tabs/styles.css"
+import classnames from "classnames"
 import style from './split.module.scss'
 
 interface SplitProps {
   tabs: string[]
+  tabProps?: TabsProps
+  buttonProps?: {
+    title: string,
+    onClick: () => void
+  }
 }
 
-const Split: React.FC<SplitProps> = ({ tabs, children }) => {
+const Split: React.FC<SplitProps> = ({ tabs, children, tabProps, buttonProps }) => {
   return (
-    <Tabs className={style.root}>
+    <Tabs className={style.root} {...tabProps} >
       <TabList className={style.tablist}>
+        {
+          buttonProps && (
+            <button
+              className={classnames(["btn btn-primary", [style.btn]])}
+              onClick={buttonProps.onClick}
+            >
+              { buttonProps.title }
+            </button>
+          )
+        }
         {
           tabs.map(x => (
             <Tab
