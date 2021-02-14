05e87b6a1824911a6213fa8e79f89c5d52995aba
theking465 <9497296+theking465@users.noreply.github.com>
2021-02-14 11:26:01 +0100

    added basic end page, added small animation on title hover

--- pages/end/[end].tsx
+++ pages/end/[end].tsx
@@ -1,9 +1,14 @@
+import Header from '@components/header/Header'
+import style from './style.module.scss'
+
 const EndPage = () => {
   return (
-    <main>
-      <h1>
-        Game over
-      </h1>
+    <main className={style.root}>
+      <Header/>
+      <div className={style.content}>
+        <h1>Game over</h1>
+        <button className={"btn btn-negative"}>Play again</button>
+      </div>
     </main>
   )
 }
