3250ef41eb3ed4ccc940ecd4e3d39e37875b0fe8
Adib Khan <adib.khan@gmail.com>
2021-03-12 01:49:52 -0500

    fix genesect

--- src/common/actions/getMini.ts
+++ src/common/actions/getMini.ts
@@ -3,6 +3,9 @@ const getMini = (sid: number): string => {
   if (sid > 15776 && sid <= 15793) {
     w = "15776" // Special Arceus Case
   }
+  if (sid > 20768 && sid <= 20772) {
+    w = "20768" // Special Genesect Case
+  }
   return `https://firebasestorage.googleapis.com/v0/b/project-grookey.appspot.com/o/mini%2F${w}.png?alt=media&token=96e08512-fff8-4233-8478-bdfb894d7bec`
 }
 
