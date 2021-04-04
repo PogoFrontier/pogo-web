6a7770945998566d11d490ad0c88dd518f95e343
theking465 <9497296+theking465@users.noreply.github.com>
2021-04-04 15:17:51 +0200

    matchmaking frontend, partial logic + UI

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router'
 import SocketContext from '@context/SocketContext'
 import style from './form.module.scss'
 import TeamContext from '@context/TeamContext'
-import { TeamMember } from '@adibkhan/pogo-web-backend'
+import { TeamMember, SearchBattlePayload } from '@adibkhan/pogo-web-backend'
 import { CODE } from '@adibkhan/pogo-web-backend/actions'
 import { v4 as uuidv4 } from 'uuid'
 import classnames from 'classnames'
@@ -16,6 +16,16 @@ const Form: React.FunctionComponent = () => {
   const team: TeamMember[] = useContext(TeamContext).team
   const [isLoading, setIsLoading] = useState(false)
   const router = useRouter()
+  // matchmaking
+  const [isMatchmaking, setIsMatchmaking] = useState(false)
+
+  // TODO: define this somewhere else, let player choose from all leagues
+  const defaultRule: any = {
+    name: 'open',
+    maxCP: 1500,
+    maxLevel: 50,
+    maxBestBuddy: 1,
+  }
 
   function joinRoom() {
     // Connected, let's sign-up for to receive messages for this room
@@ -34,6 +44,22 @@ const Form: React.FunctionComponent = () => {
         connect(uuidv4(), payload)
       }
     }
+  }
+
+  function joinQuickPlay() {
+    // determine rule
+    setIsMatchmaking(true)
+    setIsLoading(true)
+    const data = { type: CODE.matchmaking_search_battle, payload: {} }
+    socket.send(JSON.stringify(data))
+  }
+
+  function quitQuickPlay() {
+    const data = { type: CODE.matchmaking_quit, payload: {} }
+    socket.send(JSON.stringify(data))
+
+    setIsMatchmaking(false)
+    setIsLoading(false)
   }
 
   function onChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -55,7 +81,16 @@ const Form: React.FunctionComponent = () => {
         </div>
       </div>
       {isLoading ? (
-        <Loader type="TailSpin" color="#68BFF5" height={40} width={40} />
+        <div className={style.loading}>
+          <Loader type="TailSpin" color="#68BFF5" height={40} width={40} />
+          <button
+            className={classnames([style.button, 'btn', 'btn-primary'])}
+            onClick={quitQuickPlay}
+            style={isMatchmaking ? { display: 'visible' } : { display: 'none' }}
+          >
+            Quit
+          </button>
+        </div>
       ) : (
         <>
           <button
@@ -65,6 +100,14 @@ const Form: React.FunctionComponent = () => {
           >
             Play
           </button>
+
+          <button
+            className={classnames([style.button, 'btn', 'btn-primary'])}
+            onClick={joinQuickPlay}
+          >
+            Quick Play
+          </button>
+
           <br />
           {/* <button onClick={getSignInWithGooglePopup}>
             Sign In With Google
