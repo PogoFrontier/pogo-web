ba6278fd4e2e747d5fef1facf50b5d44c6402f17
Adib Khan <adib.khan@gmail.com>
2021-04-06 14:19:58 -0400

    add team ivs

--- src/context/TeamContext.tsx
+++ src/context/TeamContext.tsx
@@ -9,6 +9,12 @@ export const defaultTeam: TeamMember[] = [
     def: 108.35,
     atk: 135.31,
     cp: 1500,
+    iv: {
+      atk: 3,
+      def: 15,
+      hp: 14
+    },
+    level: 14.5,
     types: ['dragon', 'flying'],
     fastMove: 'DRAGON_BREATH',
     chargeMoves: ['DRAGON_CLAW', 'HURRICANE'],
@@ -23,6 +29,12 @@ export const defaultTeam: TeamMember[] = [
     def: 142.22,
     atk: 110.19,
     cp: 1498,
+    iv: {
+      atk: 1,
+      def: 15,
+      hp: 15
+    },
+    level: 23,
     types: ['water', 'none'],
     fastMove: 'WATER_GUN',
     chargeMoves: ['HYDRO_CANNON', 'ICE_BEAM'],
@@ -35,6 +47,12 @@ export const defaultTeam: TeamMember[] = [
     def: 110.9,
     atk: 131.55,
     cp: 1500,
+    iv: {
+      atk: 0,
+      def: 15,
+      hp: 13
+    },
+    level: 19.5,
     types: ['fire', 'flying'],
     fastMove: 'FIRE_SPIN',
     chargeMoves: ['DRAGON_CLAW', 'BLAST_BURN'],
@@ -47,6 +65,12 @@ export const defaultTeam: TeamMember[] = [
     def: 124.27,
     atk: 121.21,
     cp: 1498,
+    iv: {
+      atk: 0,
+      def: 14,
+      hp: 11
+    },
+    level: 21,
     types: ['grass', 'poison'],
     fastMove: 'VINE_WHIP',
     chargeMoves: ['FRENZY_PLANT', 'SLUDGE_BOMB'],
@@ -59,6 +83,12 @@ export const defaultTeam: TeamMember[] = [
     def: 122.09,
     atk: 119.38,
     cp: 1499,
+    iv: {
+      atk: 10,
+      def: 15,
+      hp: 13
+    },
+    level: 16.5,
     types: ['psychic', 'none'],
     fastMove: 'SHADOW_CLAW',
     chargeMoves: ['SURF', 'WILD_CHARGE'],
@@ -71,6 +101,12 @@ export const defaultTeam: TeamMember[] = [
     def: 77.4,
     atk: 179.1,
     cp: 1498,
+    iv: {
+      atk: 4,
+      def: 15,
+      hp: 15
+    },
+    level: 13,
     types: ['psychic', 'none'],
     fastMove: 'PSYCHO_CUT',
     chargeMoves: ['FLAMETHROWER', 'PSYSTRIKE'],
