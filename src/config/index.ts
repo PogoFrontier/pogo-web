adf0c8a4b9df95b1e8d5b9cb80e9b829e6a9ac34
Adib Khan <adib.khan@gmail.com>
2021-04-20 00:30:47 -0400

    fix dev env

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,10 +4,10 @@
 export const SERVER =
   process.env.NODE_ENV === 'production'
     ? 'http://pogo-web-backend.eba-gjbsq2ui.us-east-2.elasticbeanstalk.com:8080/'
-    : 'http://localhost:2999/'
+    : 'http://localhost:8081/'
 export const WSS =
   process.env.NODE_ENV === 'production'
     ? 'ws://pogo-web-backend.eba-gjbsq2ui.us-east-2.elasticbeanstalk.com:8082/'
-    : 'ws://localhost:3000/'
+    : 'ws://localhost:8088/'
 
 export const CDN_BASE_URL = 'https://d1bbfbaqrr54l0.cloudfront.net'
