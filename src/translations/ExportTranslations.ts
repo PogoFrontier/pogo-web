125740d324ca9b6c5832e660d677b3df170fd7ea
theking465 <9497296+theking465@users.noreply.github.com>
2021-05-19 21:00:25 +0200

    removed test language

--- src/translations/ExportTranslations.ts
+++ src/translations/ExportTranslations.ts
@@ -1,6 +1,5 @@
 import Dutch from './data/Dutch.json'
-import French from './data/French.json'
 import German from './data/German.json'
 import English from './data/English.json'
 
-export { Dutch, French, German, English }
+export { Dutch, German, English }
