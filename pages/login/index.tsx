9e7f935180b6bbe9ab19768af168503ae7c34ad5
unknown <jarthur.jmj@gmail.com>
2021-05-20 15:04:20 -0400

    more sign in functionality, needs testing

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,3 +1,7 @@
+import {
+  postNewGoogleUser,
+  signInWithGoogleId,
+} from '@common/actions/userAPIActions'
 import Layout from '@components/layout/Layout'
 import UserContext from '@context/UserContext'
 import React, { useContext, useEffect } from 'react'
@@ -14,15 +18,72 @@ const LoginPage = () => {
     getGoogleSignInRedirectResult().then((result) => {
       // console.log(result)
       const googleUser = result.user
-      if (googleUser) {
+      if (
+        googleUser &&
+        googleUser.uid &&
+        googleUser.displayName &&
+        googleUser.email
+      ) {
         // try to load profile via google id
-        // if no profile exists, create new account
-        setUser({
-          googleId: googleUser.uid,
-          displayName: googleUser.displayName,
-          email: googleUser.email,
-          teams: [],
-        })
+        signInWithGoogleId(googleUser.uid)
+          .then((signInResult) => {
+            const { userData, token } = signInResult.token
+            if (userData && token) {
+              // set user, save token, and store auth in local storage
+              setUser(userData)
+              localStorage.setItem(
+                'authedUser',
+                JSON.stringify({
+                  // ALSO SAVE TOKEN!!
+                  googleId: googleUser.uid,
+                  email: googleUser.email,
+                })
+              )
+            } else {
+              // corrupted data
+            }
+          })
+          .catch((err) => {
+            if (err.response && err.response.status) {
+              switch (err.response.status) {
+                case 401:
+                  // user does not exist, create a new one
+                  postNewGoogleUser({
+                    // function can also save a username/teams, if local user, ask to transfer before this
+                    uid: googleUser.uid,
+                    displayName: googleUser.displayName,
+                  })
+                    .then((newUser) => {
+                      // set user, save token, and store auth in local storage
+                      const { userData, token } = newUser.data
+                      if (userData && token) {
+                        setUser(userData)
+                        localStorage.setItem(
+                          'authedUser',
+                          JSON.stringify({
+                            // ALSO SAVE TOKEN!!
+                            googleId: googleUser.uid,
+                            email: googleUser.email,
+                          })
+                        )
+                      } else {
+                        // corrupted data
+                      }
+                    })
+                    .catch((/* err */) => {
+                      // all errors here are server error, since we know the user does not already exist
+                      // console.log(err);
+                    })
+                  break
+                case 500:
+                  // internal server error, display error message
+                  break
+                default:
+                  // other unknown error, display error message
+                  break
+              }
+            }
+          })
       } else {
         // no redirect happened, I think
       }
